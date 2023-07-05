42c21cb1601339b59c8e86e68d3453703994cc84
57840146 <chuhan1125@gmail.com>
2023-07-05 17:51:58 -0400

    update using functional body

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var blockSize = 25;
-var total_row = 17; //total row number
-var total_col = 17; //total column number
+var total_row = 33; //total row number
+var total_col = 33; //total column number 
 var board;
 var context;
  
@@ -10,22 +10,47 @@ var snakeY = blockSize * 5;
 // Set the total number of rows and columns
 var speedX = 0;  //speed of snake in x coordinate.
 var speedY = 0;  //speed of snake in Y coordinate.
+var speed = 100;
  
 var snakeBody = [];
+var snaketype =[];
+var food=[];
  
 var foodX;
 var foodY;
- 
+
 var gameOver = false;
 var score = 0;
 var bestScore = 0;
 
+var x;
+// class enemy{
+//     constructor(){
+
+//     }
+
+//     get color(){
+
+//     }
+
+//     get type(){
+
+//     }
+
+//     use(){
+
+//     }
+
+//     remove(){
+
+//     }
+// }
 class Body {
     constructor() {
       if (new.target === Body) {
         throw new TypeError("Cannot instantiate abstract class.");
       }
-      this.level = 1;
+    //   this.level = 1;
     }
 
     get color() {
@@ -35,6 +60,7 @@ class Body {
     get type() {
       throw new Error("Property 'type' must be implemented.");
     }
+
     use() {
       throw new Error("Method 'use()' must be implemented.");
     }
@@ -45,7 +71,7 @@ class Body {
     }
 
     remove(){
-        return
+        return;
     }
 }
 
@@ -55,7 +81,7 @@ class GenericBody extends Body {
     }
   
     get color() {
-      return "gray";
+      return "orange";
     }
   
     get type() {
@@ -63,7 +89,7 @@ class GenericBody extends Body {
     }
   
     get level() {
-      return this.level;
+    //   return this.level;
     }
   
     use(){
@@ -75,9 +101,7 @@ class ScoreBody extends Body{
     constructor(){
         super();
         this.scoreInterval = null;
-        
     }
-
 
     get color(){
         return "red";
@@ -88,7 +112,7 @@ class ScoreBody extends Body{
     }
 
     get level(){
-        return this.level;
+        // return this.level;
     }
 
     use(){
@@ -99,7 +123,6 @@ class ScoreBody extends Body{
 
     remove(){
         clearInterval(this.scoreInterval);
-
     }
 }
 
@@ -110,7 +133,7 @@ class MultiplierBody extends Body{
     }
 
     get color(){
-        return "green";
+        return "yellow";
     }
 
     get type(){
@@ -123,8 +146,10 @@ class MultiplierBody extends Body{
     }
 
     use(){
-        global.score = global.score * 2
-        global.speed = global.speed * 2
+        score = score * 2
+        speed = 75;
+        clearInterval()
+        setInterval(update, speed);
     }
 
     upgrade(){
@@ -151,6 +176,7 @@ class ShieldBody extends Body{
 
     use(){
         global.protected = true;
+        console.log("test");
     }
 
     remove(){
@@ -164,11 +190,24 @@ window.onload = function () {
     board.height = total_row * blockSize;
     board.width = total_col * blockSize;
     context = board.getContext("2d");
- 
+
+    // context.fillStyle = "green";
+    // context.fillRect(0, 0, board.width, board.height);
+
+    //all functional body
+    let score = new ScoreBody();
+    food.push(score);
+    let multiplier = new MultiplierBody();
+    food.push(multiplier);
+    let generic = new GenericBody();
+    food.push(generic);
+    let shield = new ShieldBody();
+    food.push(shield);
+
     placeFood();
     document.addEventListener("keyup", changeDirection);  //for movements
     // Set snake speed
-    setInterval(update, 1000 / 10);
+    setInterval(update, speed);
 }
  
 function update() {
@@ -178,25 +217,37 @@ function update() {
             score = 0;
             updateBestScore();
         }
-        let restart = window.confirm("Game over! Do you want to restart the game?");//back to starting page
+        let restart = window.confirm("Game over!");//back to starting page
         if(restart){
             history.back();
         }
         // return;
     }
- 
+    // console.log(speed);
     // Background of a Game
     context.fillStyle = "green";
     context.fillRect(0, 0, board.width, board.height);
- 
+    
     // Set food color and position
-    context.fillStyle = "yellow";
+    // const test=new ScoreBody();
+    // context.fillStyle = test.color;
+    // context.fillRect(foodX, foodY, blockSize, blockSize);
+    // context.fillStyle=food[Math.floor(Math.random()*3)].color;
+    // context.fillRect(foodX, foodY, blockSize, blockSize);
+    // x=food[Math.floor(Math.random()*4)].color;
+
+    //generate body
+    context.fillStyle=x.color;
     context.fillRect(foodX, foodY, blockSize, blockSize);
- 
+
     if (snakeX == foodX && snakeY == foodY) {
         snakeBody.push([foodX, foodY]);
+        snaketype.push(x);
+        x.use();
         placeFood();
         score++;
+        // test.type;
+        // test.use;
         updateScore();
     }
  
@@ -214,12 +265,13 @@ function update() {
     snakeY += speedY * blockSize;  //updating Snake position in Y coordinate.
     context.fillRect(snakeX, snakeY, blockSize, blockSize);
 
-    context.fillStyle = "black";
     for (let i = 0; i < snakeBody.length; i++) {
+        // console.log(snaketype[i]);
+        context.fillStyle = snaketype[i].color;
         context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
     }
  
-        if (snakeX < 0) {
+    if (snakeX < 0) {
         snakeX = (total_col - 1) * blockSize;
     } else if (snakeX >= total_col * blockSize) {
         snakeX = 0;
@@ -239,27 +291,28 @@ function update() {
             // alert("Game Over");
         }
     }
+    
 }
  
 // Movement of the Snake - We are using addEventListener
 function changeDirection(e) {
-    if (e.code == "ArrowUp" && speedY != 1) {
+    if (e.code == "KeyW" && speedY != 1) {
         // If up arrow key pressed with this condition...
         // snake will not move in the opposite direction
         speedX = 0;
         speedY = -1;
     }
-    else if (e.code == "ArrowDown" && speedY != -1) {
+    else if (e.code == "KeyS" && speedY != -1) {
         //If down arrow key pressed
         speedX = 0;
         speedY = 1;
     }
-    else if (e.code == "ArrowLeft" && speedX != 1) {
+    else if (e.code == "KeyA" && speedX != 1) {
         //If left arrow key pressed
         speedX = -1;
         speedY = 0;
     }
-    else if (e.code == "ArrowRight" && speedX != -1) {
+    else if (e.code == "KeyD" && speedX != -1) {
         //If Right arrow key pressed
         speedX = 1;
         speedY = 0;
@@ -279,10 +332,15 @@ function updateBestScore(){
  
 // Randomly place food
 function placeFood() {
- 
+    // x=food[Math.floor(Math.random()*4)];// which body to generate
+    x=food[1];
+    // console.log(x);
+    // context.fillStyle=x;
     // in x coordinates.
     foodX = Math.floor(Math.random() * total_col) * blockSize;
-     
+    
     //in y coordinates.
     foodY = Math.floor(Math.random() * total_row) * blockSize;
+    
+    // context.fillRect(foodX, foodY, blockSize, blockSize);
 }
