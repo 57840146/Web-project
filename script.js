2a78b31683a535e7ed5eaca30e550af0389c0715
57840146 <chuhan1125@gmail.com>
2023-07-10 23:35:39 -0400

    update user can use arrow up down right left to control the snake as well

--- script.js
+++ script.js
@@ -672,22 +672,42 @@ function update() {
 // Movement of the Snake - We are using addEventListener
 function changeDirection(e) {
     if (e.code == "KeyW" && speedY != 1) {
-        // If up arrow key pressed with this condition...
+        // If W key pressed with this condition...
         // snake will not move in the opposite direction
         speedX = 0;
         speedY = -1;
     }
     else if (e.code == "KeyS" && speedY != -1) {
-        //If down arrow key pressed
+        //If S key pressed
         speedX = 0;
         speedY = 1;
     }
     else if (e.code == "KeyA" && speedX != 1) {
-        //If left arrow key pressed
+        //If A key pressed
         speedX = -1;
         speedY = 0;
     }
     else if (e.code == "KeyD" && speedX != -1) {
+        //If D key pressed
+        speedX = 1;
+        speedY = 0;
+    }else if (e.code == "ArrowUp" && speedY != 1) {
+        // If up arrow key pressed with this condition...
+        // snake will not move in the opposite direction
+        speedX = 0;
+        speedY = -1;
+    }
+    else if (e.code == "ArrowDown" && speedY != -1) {
+        //If down arrow key pressed
+        speedX = 0;
+        speedY = 1;
+    }
+    else if (e.code == "ArrowLeft" && speedX != 1) {
+        //If left arrow key pressed
+        speedX = -1;
+        speedY = 0;
+    }
+    else if (e.code == "ArrowRight" && speedX != -1) {
         //If Right arrow key pressed
         speedX = 1;
         speedY = 0;
